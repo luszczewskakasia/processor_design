d78baf5455cf197f6cdaa3806b78488c716c1bb1
Katarzyna Luszczewska <luszczewskakasia>
2022-11-01 11:10:46 +0100

    control_lines_prep

--- control unit/contro_i_o.vhd
+++ control unit/contro_i_o.vhd
@@ -4,29 +4,29 @@ USE IEEE.std_logic_1164.ALL;
 USE IEEE.numeric_std.ALL;
 ENTITY control_unit IS
   PORT (
-    instr_reg   	: IN std_logic_vector(7 DOWNTO 0);
-    debug    		: IN std_logic_vector(2 DOWNTO 0); 
-    status_bit_reg	: IN std_logic;
-    clk     		: IN std_logic;
-    reset		: IN std_logic;
+    instr_reg   		: IN std_logic_vector(7 DOWNTO 0);
+    debug    			: IN std_logic_vector(2 DOWNTO 0); 
+    status_bit_reg		: IN std_logic;
+    clk     			: IN std_logic;
+    reset				: IN std_logic;
 
-    demux_mem 		: OUT std_logic;
-    demux_A		: OUT std_logic;
-    demux_B		: OUT std_logic_vector (1 DOWNTO 0);
+    demux_mem 			: OUT std_logic;
+    demux_A				: OUT std_logic;
+    demux_B				: OUT std_logic_vector (1 DOWNTO 0);
 
-    mux_mem		: OUT std_logic_vector (1 DOWNTO 0);
-    mux_reg		: OUT std_logic_vector (1 DOWNTO 0);
+    mux_mem				: OUT std_logic_vector (1 DOWNTO 0);
+    mux_reg				: OUT std_logic_vector (1 DOWNTO 0);
 
-    address_add 	: OUT std_logic_vector (1 DOWNTO 0);	--adder
-    enable_instr 	: OUT std_logic; 			--instruction
+    address_add 		: OUT std_logic_vector (1 DOWNTO 0);	--adder
+    enable_instr 		: OUT std_logic; 						--instruction
     enable_status_bit 	: OUT std_logic;
 
-    alu_instr		: OUT std_logic_vector (2 DOWNTO 0);
-    rw_mem_off		: OUT std_logic_vector (1 DOWNTO 0);
+    alu_instr			: OUT std_logic_vector (2 DOWNTO 0);
+    rw_mem_off			: OUT std_logic_vector (1 DOWNTO 0);
 
-    rw_reg_off		: OUT std_logic_vector (1 DOWNTO 0);
-    address_A_reg	: OUT std_logic_vector (3 DOWNTO 0);
-    address_B_reg	: OUT std_logic_vector (3 DOWNTO 0)
+    rw_reg_off			: OUT std_logic_vector (1 DOWNTO 0);
+    address_A_reg		: OUT std_logic_vector (3 DOWNTO 0);
+    address_B_reg		: OUT std_logic_vector (3 DOWNTO 0)
     
   );
 END ENTITY control_unit;
@@ -35,8 +35,7 @@ architecture bhv of control_unit is
 begin
   PROCESS(clk,reset)
 
-	function control_lines (temp_instr_reg:  std_logic_vector (7 DOWNTO 0); temp_clock_cycle : integer; temp_status_bit : std_logic) return std_logic_vector is
-	
+	function control_lines_instr (temp_instr_reg:  std_logic_vector (7 DOWNTO 0); temp_clock_cycle : integer; temp_status_bit : std_logic) return std_logic_vector is
 	BEGIN
       		CASE temp_clock_cycle IS
         	WHEN 1 =>
@@ -121,8 +120,19 @@ begin
 			end case;
         	WHEN OTHERS => RETURN "----------------------------";      
       END CASE;
-    END control_lines;
-
+    END control_lines_instr;
+	
+	function control_lines_prep (temp_clock_cycle : integer) return std_logic_vector is
+	begin
+	case temp_clock_cycle is
+		when 1 => return '0' & '0' & "00" & "00" & "00" & "00" & '0' & '0' & "000" & "00" & "10" & "0000" & "1000" & '0';
+		when 2 => return '0' & '0' & "10" & "00" & "00" & "00" & '1' & '0' & "000" & "10" & "00" & "0000" & "0000" & '0';
+		when 3 => return '0' & '0' & "00" & "00" & "00" & "00" & '0' & '0' & "000" & "00" & "10" & "1000" & "0111" & '0';
+		when 4 => return '0' & '0' & "00" & "00" & "00" & "00" & '0' & '0' & "000" & "00" & "00" & "0000" & "0000" & '0';
+		when 5 => return '0' & '0' & "00" & "00" & "00" & "00" & '0' & '0' & "000" & "00" & "11" & "1000" & "0000" & '1';
+		WHEN OTHERS => RETURN "----------------------------";
+	end case;
+	end control_lines_prep;
   BEGIN
     -- NO VHDL CODE HERE
     IF reset='0' THEN
