3ed21645dba5625ae9f79986609dfb4e18641ea5
stevenadisaputra <117271182+stevenadisaputra@users.noreply.github.com>
2022-11-02 16:59:55 +0100

    Update register_C.vhd

--- intermediate stage/register_C.vhd
+++ intermediate stage/register_C.vhd
@@ -5,6 +5,7 @@ Entity register_C Is
 Port(Data_In : IN std_logic_vector(18 DOWNTO 0);
      clk : IN std_logic;
      reset : IN std_logic;
+     ctrl : IN std_logic;
      Data_Out : OUT std_logic_vector(18 DOWNTO 0));
 End register_C;
 
@@ -12,11 +13,12 @@ Architecture bhv of register_C Is
 begin
 process(clk,ctrl)
 begin 
-     if rising_edge(clk) then
-       if(ctrl ='1') then 
+     if(reset='0') then
+	Data_Out<=(OTHERS=>'0');
+     elsif rising_edge(clk) then
+       if(ctrl ='0') then 
           Data_Out<= Data_In;
-       elsif(reset ='1') then 
-          Data_Out<="00000000";
+       
        end if;
      end if;
 end process;
