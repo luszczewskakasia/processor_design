cfacbe340e5c8720cae61bf29e1a50646ac296ae
shinwoon kim <117097698+ythekey@users.noreply.github.com>
2022-11-05 13:33:56 +0100

    Update mux_register.vhd

--- computer system/mux_register.vhd
+++ computer system/mux_register.vhd
@@ -5,16 +5,17 @@ Entity mux_register Is
 Port( ALU : IN std_logic_vector(18 DOWNTO 0);
       B_register : IN std_logic_vector(18 DOWNTO 0);
       load_register : IN std_logic_vector(18 DOWNTO 0);
+      dummy : IN std_logic_vector (18 downto 0);
       ctrl : IN std_logic_vector(1 DOWNTO 0);
       b : OUT std_logic_vector(18 DOWNTO 0));
 End Entity mux_register;
 
 Architecture bhv of mux_register Is
-signal dash : std_logic_vector(18 DOWNTO 0):="-------------------";
+
 begin
      b <= ALU when (ctrl="00") else
           B_register when (ctrl="01") else
           load_register when (ctrl="10") else
-          dash when (ctrl="11");
+          dummy when (ctrl="11");
 
 End bhv;
